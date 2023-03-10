d81b2abce6bbe27ea32287e99400ccfb1d664e24
AlexShuster <aps83@pitt.edu>
2023-03-10 16:22:00 -0500

    Initial app setup (Hello World!)

--- DopplerRadarApp/DopplerRadarApp/App.xaml.cs
+++ DopplerRadarApp/DopplerRadarApp/App.xaml.cs
@@ -13,7 +13,6 @@ namespace DopplerRadarApp
         {
             InitializeComponent();
 
-            DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
         }
 
