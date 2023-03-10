55636863777f92d0db9ba74ab86050b059d121a7
AlexShuster <aps83@pitt.edu>
2023-03-10 14:44:08 -0500

    Add project files.

---
+++ DopplerRadarApp/DopplerRadarApp/Models/DopplerRadarBLE.cs
@@ -0,0 +1,28 @@
+﻿using Android.Bluetooth;
+using BluetoothLE.Core;
+using BluetoothLE.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DopplerRadarApp.Models
+{
+    class DopplerRaderBLE
+    {
+        IAdapter BluetoothAdapter;
+
+        // List of Discovered Devices
+        //List<var> 
+
+        //// BluetoothAdapter.StartScanningForDevices();   Call when san button is pressed
+
+
+        //private void DeviceDiscovered(object sender, DeviceDiscoveredEventArgs e)
+        //{
+        //    if (DeviceList.All(x => x.Id != e.Device.Id))
+        //    {
+        //        DeviceList.Add(e.Device);
+        //    }
+        //}
+    }
+}
